84e470dc8a8023efa0e09b6c4f8c4248c2ffa429
Sahil pawar <sahilpawar3033@gmail.com>
2025-08-12 12:43:27 +0530

    Add files via upload

---
+++ C Language/Assignment/Assign 9/upper.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+ char str[20]="Akanksha";
+ strupr(str);
+printf("%s",str);
+
+// op:AKANKSHA
+
+char str[20]="Akanksha";
+strlwr(str);
+printf("%s",str);
+
+//  op:akanksha
+
+}
