c272cb3a5729839938912f166640fecfd3dcaf3f
Sahil pawar <sahilpawar3033@gmail.com>
2025-08-12 12:45:41 +0530

    upper.c

--- C Language/Assignment/Assign 9/upper.c
+++ C Language/Assignment/Assign 9/upper.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 #include<string.h>
 void main(){
- char str[20]="Akanksha";
+ char str[20]="Sahil";
  strupr(str);
 printf("%s",str);
 
 // op:AKANKSHA
 
-char str[20]="Akanksha";
+char str[20]="Pawar";
 strlwr(str);
 printf("%s",str);
 
 //  op:akanksha
+
 
 }
